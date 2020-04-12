f32cee086178a21aa7e63f2567977125b23de7c7
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-12 11:11:50 +0200

    Divide preparation into copying and preprocesing

---
+++ prepare_datasets/get_datasets.bash
@@ -0,0 +1,14 @@
+mkmount
+cd /Users/maudgautier/Documents/These/R_projects/07_Github_R_projects/ABC-recombinants
+
+# SEQ 2
+cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Sequencing_2/06_Analyses_of_Recombinants/02_Recombinants_and_False_Positives_Dataset/Recombinants_MUT_dataset.txt ./data/MUT_seq2.txt
+cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Sequencing_2/06_Analyses_of_Recombinants/02_Recombinants_and_False_Positives_Dataset/Recombinants_WT_dataset.txt ./data/WT_seq2.txt
+
+# SEQ 1
+cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Sequencing_1/05_Analyses_of_Recombinants/02_Recombinants_and_False_Positives_Dataset/Recombinants_MUT_dataset.txt ./data/MUT_seq1.txt
+cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Sequencing_1/05_Analyses_of_Recombinants/02_Recombinants_and_False_Positives_Dataset/Recombinants_WT_dataset.txt ./data/WT_seq1.txt
+
+# Simulations
+cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Simulations_ABC_for_HFM1/Results/Perform_ABC/Summary_statistics.txt ./simulations/Summary_statistics.txt
+
--- prepare_datasets/preprocessing.bash
+++ prepare_datasets/preprocessing.bash
@@ -1,13 +1,3 @@
-cd /Users/maudgautier/Documents/These/R_projects/07_Github_R_projects/ABC-recombinants
-
-# SEQ 2
-cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Sequencing_2/06_Analyses_of_Recombinants/02_Recombinants_and_False_Positives_Dataset/Recombinants_MUT_dataset.txt ./data/MUT_seq2.txt
-cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Sequencing_2/06_Analyses_of_Recombinants/02_Recombinants_and_False_Positives_Dataset/Recombinants_WT_dataset.txt ./data/WT_seq2.txt
-
-# SEQ 1
-cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Sequencing_1/05_Analyses_of_Recombinants/02_Recombinants_and_False_Positives_Dataset/Recombinants_MUT_dataset.txt ./data/MUT_seq1.txt
-cp /Users/maudgautier/Documents/These/02_pandata_sshfs/5_HFM1/Sequencing_1/05_Analyses_of_Recombinants/02_Recombinants_and_False_Positives_Dataset/Recombinants_WT_dataset.txt ./data/WT_seq1.txt
-
 # Extract each sample individually
 (head -n1 ./data/WT_seq1.txt && grep S28355 ./data/WT_seq1.txt) > ./data/WT_S28355.txt
 (head -n1 ./data/WT_seq1.txt && grep S28371 ./data/WT_seq1.txt) > ./data/WT_S28371.txt
